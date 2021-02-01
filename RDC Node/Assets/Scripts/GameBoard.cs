7af1592adab68dfebdd7dc50e38c7eaa97787625
AdamBJohnson <49881131+AdamBJohnson@users.noreply.github.com>
2021-02-01 13:51:38 -0600

    Add Tile constructor

--- RDC Node/Assets/Scripts/GameBoard.cs
+++ RDC Node/Assets/Scripts/GameBoard.cs
@@ -6,6 +6,7 @@ public class GameBoard : MonoBehaviour
 {
     public enum Player
     {
+        None = 0,
         Player1 = 1,
         player2 = 2
     }
@@ -22,7 +23,8 @@ public class GameBoard : MonoBehaviour
         Red = 0,
         Blue = 1,
         Green = 2,
-        Yellow = 3
+        Yellow = 3,
+        None = 4
     }
 
     public struct Coordinate
@@ -36,11 +38,38 @@ public class GameBoard : MonoBehaviour
         public Coordinate coord;
         public Player player;
         public PieceType pieceType;
+        public GamePiece[] adjacentPieces = new GamePiece[8];
     }
 
     public class Tile : GamePiece
     {
         public ResourceType ResourceType;
         public int maxLoad;
+
+        public Tile(ResourceType r, int max, Player p = None, Coordinate c = null)
+        {
+            pieceType = Tile;
+            ResourceType = r;
+            maxLoad = max;
+            player = p;
+            coord = c;
+        }
     }
+
+    public Tile[] GameTiles = new Tile[] 
+    {
+        new Tile(Red, 1), 
+        new Tile(Red, 2), 
+        new Tile(Red, 3),
+        new Tile(Blue, 1),
+        new Tile(Blue, 2),
+        new Tile(Blue, 3),
+        new Tile(Green, 1),
+        new Tile(Green, 2),
+        new Tile(Green, 3),
+        new Tile(Yellow, 1),
+        new Tile(Yellow, 2),
+        new Tile(Yellow, 3),
+        new Tile(None, 0)
+    };
 }
