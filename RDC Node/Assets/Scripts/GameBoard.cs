73a7472c6715985ecd18306664b85737e3a85dbf
FairyTamerRyles <54481765+FairyTamerRyles@users.noreply.github.com>
2021-02-10 19:45:20 -0600

    Added missing files from the gamecore

---
+++ RDC Node/Assets/Scripts/GameBoard.cs
@@ -0,0 +1,250 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static System.Math;
+
+public class GameBoard
+{
+
+    private float rnd;
+
+    public void Start()
+    {
+        //Hi
+    }
+
+    const int numResources = 4;
+
+    int[] player1Resources;
+    int[] player2Resources;
+
+    public GamePiece[,] gameBoard;
+
+    public enum Player
+    {
+        None = 0,
+        Player1 = 1,
+        Player2 = 2
+    }
+
+    public enum PieceType
+    {
+        Tile = 0,
+        Branch = 1,
+        Node = 2
+    }
+
+    public enum ResourceType
+    {
+        Red = 0,
+        Blue = 1,
+        Green = 2,
+        Yellow = 3,
+        None = 4
+    }
+
+    public struct Coordinate
+    {
+        public int x;
+        public int y;
+    }
+
+    public class GamePiece
+    {
+        public Coordinate coord;
+        public Player player;
+        public PieceType pieceType;
+        //public GamePiece[] adjacentPieces = new GamePiece[8];
+
+        public GamePiece(Coordinate c, PieceType pt)
+        {
+            player = Player.None;
+            coord = c;
+            pieceType = pt;
+        }
+    }
+
+    public class Tile : GamePiece
+    {
+        public ResourceType ResourceType;
+        public int maxLoad;
+
+        public Tile(ResourceType r, int max): base(new Coordinate() {x = 0, y = 0}, PieceType.Tile)
+        {
+            ResourceType = r;
+            maxLoad = max;
+            player = Player.None;
+        }
+    }
+
+    public class Move
+    {
+        int[] resourceChange;
+        Player player;
+
+        public Move(int[] rChange, Player p)
+        {
+            resourceChange = rChange;
+            player = p;
+        }
+    }
+
+    public class PlacePiece : Move
+    {
+        Coordinate coord;
+        public PlacePiece(int[] rChange, Player p, Coordinate c) : base(rChange, p)
+        {
+            coord = c;
+        }
+    }
+
+    //TODO: Try to make this const later
+    private List<Coordinate> tileIndexes = new List<Coordinate> 
+    {
+        new Coordinate{x = 1, y = 5}, 
+        new Coordinate{x = 3, y = 3}, 
+        new Coordinate{x = 3, y = 5}, 
+        new Coordinate{x = 3, y = 7}, 
+        new Coordinate{x = 5, y = 1}, 
+        new Coordinate{x = 5, y = 3}, 
+        new Coordinate{x = 5, y = 5}, 
+        new Coordinate{x = 5, y = 7}, 
+        new Coordinate{x = 5, y = 9}, 
+        new Coordinate{x = 7, y = 3}, 
+        new Coordinate{x = 7, y = 5}, 
+        new Coordinate{x = 7, y = 7}, 
+        new Coordinate{x = 9, y = 5}
+    };
+
+    public List<Tile> GameTiles = new List<Tile> 
+    {
+        new Tile(ResourceType.Red, 1), 
+        new Tile(ResourceType.Red, 2), 
+        new Tile(ResourceType.Red, 3),
+        new Tile(ResourceType.Blue, 1),
+        new Tile(ResourceType.Blue, 2),
+        new Tile(ResourceType.Blue, 3),
+        new Tile(ResourceType.Green, 1),
+        new Tile(ResourceType.Green, 2),
+        new Tile(ResourceType.Green, 3),
+        new Tile(ResourceType.Yellow, 1),
+        new Tile(ResourceType.Yellow, 2),
+        new Tile(ResourceType.Yellow, 3),
+        new Tile(ResourceType.None, -1)
+    };
+
+    public GameBoard()
+    {
+        gameBoard = generateBoard();
+        player1Resources = new int[4] {0, 0, 0, 0};
+        player2Resources = new int[4] {0, 0, 0, 0};
+    }
+
+    public int getScore(Player p)
+    {
+        return numberOfNodes(p) + numberCapturedTiles(p) + longestNetwork(p);
+    }
+
+    private int numberOfNodes(Player p)
+    {
+        return 0;
+    }
+
+    private int numberCapturedTiles(Player p)
+    {
+        return 0;
+    }
+
+    private int longestNetwork(Player p)
+    {
+        return 0;
+    }
+
+    public void makeMove(Move[] moves)
+    {
+        
+    }
+
+    public Player checkForWin()
+    {
+        if(getScore(Player.Player1) >= 10)
+        {
+            return Player.Player1;
+        }
+        else if (getScore(Player.Player2) >= 10)
+        {
+            return Player.Player1;
+        }
+        return Player.None;
+    }
+
+    public bool isValidMove(Move m)
+    {
+        return true;
+    }
+
+    private GamePiece[,] generateBoard()
+    {
+        GamePiece[,] newBoard = new GamePiece[11, 11];
+
+        //Randomizes order of the tiles
+        List<bool> selectedPieces = new List<bool> {true, true, true, true, true, true, true, true, true, true, true, true, true};
+        List<GamePiece> randomizedGameTiles = new List<GamePiece>();
+        while(selectedPieces.Contains(true))
+        {
+            rnd = Random.Range(0.0f, 13.0f);
+            int newIndex = (int)(Floor(rnd));
+            if(selectedPieces[newIndex])
+            {
+                randomizedGameTiles.Add(GameTiles[newIndex]);
+                selectedPieces[newIndex] = false;
+            }
+
+        }
+
+        //Initialize board by looping over the tile positions and placing the randomized tiles in them
+        //and then surrounding them with new GamePieces
+        PieceType pt;
+        for(int i = 0; i < tileIndexes.Count; ++i)
+        {
+            newBoard[tileIndexes[i].x, tileIndexes[i].y] = randomizedGameTiles[i];
+            newBoard[tileIndexes[i].x, tileIndexes[i].y].coord = tileIndexes[i];
+
+            for(int row = tileIndexes[i].x - 1; row <= tileIndexes[i].x + 1; ++row)
+            {
+                for(int col = tileIndexes[i].y - 1; col <= tileIndexes[i].y + 1; ++col)
+                {
+                    if(newBoard[row,col] == null)
+                    {
+                        if(col % 2 == 0 && row % 2 == 0)
+                        {
+                            pt = PieceType.Node;
+                            newBoard[row,col] = new GamePiece(new Coordinate{x = row, y = col}, pt);
+                        }
+                        else
+                        {
+                            pt = PieceType.Branch;
+                            newBoard[row,col] = new GamePiece(new Coordinate{x = row, y = col}, pt);
+                        }
+                    }
+                }
+            }
+        }
+
+        /*for(int i = 0; i < 11; ++i)
+        {
+            for(int j = 0; j < 11; ++j)
+            {
+                if(newBoard[i,j] != null){ Debug.Log(newBoard[i,j].pieceType);}
+                else {Debug.Log("0");}
+            }
+        }*/
+
+        return newBoard;
+    }
+
+    private GamePiece[,] generateBoard(string boardSeed)
+    {
+        return new GamePiece[11, 11];
+    }
+}
---
+++ RDC Node/Assets/Scripts/GameController.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private GameBoard gameBoard;
+    private AI testAI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameBoard = new GameBoard();
+        testAI = new AI();
+        
+        GameBoard.GamePiece[,] testFullEnclosure = {{null, null, null, null, new GameBoard.GamePiece(new GameBoard.Coordinate{x = 0, y = 4}, GameBoard.PieceType.Node),  new GameBoard.GamePiece(new GameBoard.Coordinate{x = 0, y = 5}, GameBoard.PieceType.Branch), new GameBoard.GamePiece(new GameBoard.Coordinate{x = 0, y = 6}, GameBoard.PieceType.Node), null, null, null, null},
+                                                    {null, null, null, null, new GameBoard.GamePiece(new GameBoard.Coordinate{x = 1, y = 4}, GameBoard.PieceType.Branch), new GameBoard.Tile(GameBoard.ResourceType.Red, 1), new GameBoard.GamePiece(new GameBoard.Coordinate{x = 1, y = 6}, GameBoard.PieceType.Branch), null, null, null, null},
+                                                    {null, null, new GameBoard.GamePiece(new GameBoard.Coordinate{x = 2, y = 2}, GameBoard.PieceType.Node),  new GameBoard.GamePiece(new GameBoard.Coordinate{x = 2, y = 3}, GameBoard.PieceType.Branch), new GameBoard.GamePiece(new GameBoard.Coordinate{x = 2, y = 4}, GameBoard.PieceType.Node), new GameBoard.GamePiece(new GameBoard.Coordinate{x = 2, y = 5}, GameBoard.PieceType.Branch), new GameBoard.GamePiece(new GameBoard.Coordinate{x = 2, y = 6}, GameBoard.PieceType.Node), new GameBoard.GamePiece(new GameBoard.Coordinate{x = 2, y = 7}, GameBoard.PieceType.Branch), new GameBoard.GamePiece(new GameBoard.Coordinate{x = 2, y = 8}, GameBoard.PieceType.Node), null, null},
+                                                    {null, null, new GameBoard.GamePiece(new GameBoard.Coordinate{x = 3, y = 2}, GameBoard.PieceType.Branch),  new GameBoard.Tile(GameBoard.ResourceType.Red, 2), new GameBoard.GamePiece(new GameBoard.Coordinate{x = 3, y = 4}, GameBoard.PieceType.Branch), new GameBoard.Tile(GameBoard.ResourceType.Blue, 3), new GameBoard.GamePiece(new GameBoard.Coordinate{x = 3, y = 6}, GameBoard.PieceType.Branch), new GameBoard.Tile(GameBoard.ResourceType.Green, 3), new GameBoard.GamePiece(new GameBoard.Coordinate{x = 3, y = 8}, GameBoard.PieceType.Branch), null, null},
+                                                    {new GameBoard.GamePiece(new GameBoard.Coordinate{x = 4, y = 0}, GameBoard.PieceType.Node),  new GameBoard.GamePiece(new GameBoard.Coordinate{x = 4, y = 1}, GameBoard.PieceType.Branch), new GameBoard.GamePiece(new GameBoard.Coordinate{x = 4, y = 2}, GameBoard.PieceType.Node),  new GameBoard.GamePiece(new GameBoard.Coordinate{x = 4, y = 3}, GameBoard.PieceType.Branch), new GameBoard.GamePiece(new GameBoard.Coordinate{x = 4, y = 4}, GameBoard.PieceType.Node), new GameBoard.GamePiece(new GameBoard.Coordinate{x = 4, y = 5}, GameBoard.PieceType.Branch), new GameBoard.GamePiece(new GameBoard.Coordinate{x = 4, y = 6}, GameBoard.PieceType.Node), new GameBoard.GamePiece(new GameBoard.Coordinate{x = 4, y = 7}, GameBoard.PieceType.Branch), new GameBoard.GamePiece(new GameBoard.Coordinate{x = 4, y = 8}, GameBoard.PieceType.Node), new GameBoard.GamePiece(new GameBoard.Coordinate{x = 4, y = 9}, GameBoard.PieceType.Branch), new GameBoard.GamePiece(new GameBoard.Coordinate{x = 4, y = 10}, GameBoard.PieceType.Node)},
+                                                    {new GameBoard.GamePiece(new GameBoard.Coordinate{x = 5, y = 0}, GameBoard.PieceType.Branch),  new GameBoard.Tile(GameBoard.ResourceType.Red, 3), new GameBoard.GamePiece(new GameBoard.Coordinate{x = 5, y = 2}, GameBoard.PieceType.Branch),  new GameBoard.Tile(GameBoard.ResourceType.Green, 1), new GameBoard.GamePiece(new GameBoard.Coordinate{x = 5, y = 4}, GameBoard.PieceType.Branch), new GameBoard.Tile(GameBoard.ResourceType.Yellow, 1), new GameBoard.GamePiece(new GameBoard.Coordinate{x = 5, y = 6}, GameBoard.PieceType.Branch), new GameBoard.Tile(GameBoard.ResourceType.Yellow, 3), new GameBoard.GamePiece(new GameBoard.Coordinate{x = 5, y = 8}, GameBoard.PieceType.Branch), new GameBoard.Tile(GameBoard.ResourceType.None, -1), new GameBoard.GamePiece(new GameBoard.Coordinate{x = 5, y = 8}, GameBoard.PieceType.Branch)},
+                                                    {new GameBoard.GamePiece(new GameBoard.Coordinate{x = 6, y = 0}, GameBoard.PieceType.Node),  new GameBoard.GamePiece(new GameBoard.Coordinate{x = 6, y = 1}, GameBoard.PieceType.Branch), new GameBoard.GamePiece(new GameBoard.Coordinate{x = 6, y = 2}, GameBoard.PieceType.Node),  new GameBoard.GamePiece(new GameBoard.Coordinate{x = 6, y = 3}, GameBoard.PieceType.Branch), new GameBoard.GamePiece(new GameBoard.Coordinate{x = 6, y = 4}, GameBoard.PieceType.Node), new GameBoard.GamePiece(new GameBoard.Coordinate{x = 6, y = 5}, GameBoard.PieceType.Branch), new GameBoard.GamePiece(new GameBoard.Coordinate{x = 6, y = 6}, GameBoard.PieceType.Node), new GameBoard.GamePiece(new GameBoard.Coordinate{x = 6, y = 7}, GameBoard.PieceType.Branch), new GameBoard.GamePiece(new GameBoard.Coordinate{x = 6, y = 8}, GameBoard.PieceType.Node), new GameBoard.GamePiece(new GameBoard.Coordinate{x = 6, y = 9}, GameBoard.PieceType.Branch), new GameBoard.GamePiece(new GameBoard.Coordinate{x = 6, y = 10}, GameBoard.PieceType.Node)},
+                                                    {null, null, new GameBoard.GamePiece(new GameBoard.Coordinate{x = 7, y = 2}, GameBoard.PieceType.Branch),  new GameBoard.Tile(GameBoard.ResourceType.Blue, 1), new GameBoard.GamePiece(new GameBoard.Coordinate{x = 7, y = 4}, GameBoard.PieceType.Branch), new GameBoard.Tile(GameBoard.ResourceType.Green, 2), new GameBoard.GamePiece(new GameBoard.Coordinate{x = 7, y = 6}, GameBoard.PieceType.Branch), new GameBoard.Tile(GameBoard.ResourceType.Yellow, 2), new GameBoard.GamePiece(new GameBoard.Coordinate{x = 7, y = 8}, GameBoard.PieceType.Branch), null, null},
+                                                    {null, null, new GameBoard.GamePiece(new GameBoard.Coordinate{x = 8, y = 2}, GameBoard.PieceType.Node),  new GameBoard.GamePiece(new GameBoard.Coordinate{x = 8, y = 3}, GameBoard.PieceType.Branch), new GameBoard.GamePiece(new GameBoard.Coordinate{x = 8, y = 4}, GameBoard.PieceType.Node), new GameBoard.GamePiece(new GameBoard.Coordinate{x = 8, y = 5}, GameBoard.PieceType.Branch), new GameBoard.GamePiece(new GameBoard.Coordinate{x = 8, y = 6}, GameBoard.PieceType.Node), new GameBoard.GamePiece(new GameBoard.Coordinate{x = 8, y = 7}, GameBoard.PieceType.Branch), new GameBoard.GamePiece(new GameBoard.Coordinate{x = 8, y = 8}, GameBoard.PieceType.Node), null, null},
+                                                    {null, null, null, null, new GameBoard.GamePiece(new GameBoard.Coordinate{x = 9, y = 4}, GameBoard.PieceType.Branch), new GameBoard.Tile(GameBoard.ResourceType.Blue, 2), new GameBoard.GamePiece(new GameBoard.Coordinate{x = 9, y = 6}, GameBoard.PieceType.Branch), null, null, null, null},
+                                                    {null, null, null, null, new GameBoard.GamePiece(new GameBoard.Coordinate{x = 10, y = 4}, GameBoard.PieceType.Node),  new GameBoard.GamePiece(new GameBoard.Coordinate{x = 10, y = 5}, GameBoard.PieceType.Branch), new GameBoard.GamePiece(new GameBoard.Coordinate{x = 10, y = 6}, GameBoard.PieceType.Node), null, null, null, null}};
+        testFullEnclosure[1, 5].coord.x = 1;
+        testFullEnclosure[1, 5].coord.y = 5;
+        testFullEnclosure[3, 5].coord.x = 3;
+        testFullEnclosure[3, 5].coord.y = 5;
+        testFullEnclosure[3, 3].coord.x = 3;
+        testFullEnclosure[3, 3].coord.y = 3;
+        testFullEnclosure[3, 7].coord.x = 3;
+        testFullEnclosure[3, 7].coord.y = 7;
+        testFullEnclosure[5, 1].coord.x = 5;
+        testFullEnclosure[5, 1].coord.y = 1;
+        testFullEnclosure[5, 3].coord.x = 5;
+        testFullEnclosure[5, 3].coord.y = 3;
+        testFullEnclosure[5, 5].coord.x = 5;
+        testFullEnclosure[5, 5].coord.y = 5;
+        testFullEnclosure[5, 7].coord.x = 5;
+        testFullEnclosure[5, 7].coord.y = 7;
+        testFullEnclosure[5, 9].coord.x = 5;
+        testFullEnclosure[5, 9].coord.y = 9;
+        testFullEnclosure[7, 3].coord.x = 7;
+        testFullEnclosure[7, 3].coord.y = 3;
+        testFullEnclosure[7, 5].coord.x = 7;
+        testFullEnclosure[7, 5].coord.y = 5;
+        testFullEnclosure[7, 7].coord.x = 7;
+        testFullEnclosure[7, 7].coord.y = 7;
+        testFullEnclosure[9, 5].coord.x = 9;
+        testFullEnclosure[9, 5].coord.y = 5;
+
+        testAI.AIGameBoard.gameBoard = testFullEnclosure;
+        
+        //branches around 5 - 1
+        testFullEnclosure[5, 0].player = GameBoard.Player.Player1;
+        testFullEnclosure[4, 1].player = GameBoard.Player.Player1;
+        testFullEnclosure[6, 1].player = GameBoard.Player.Player1;
+        //branches around 3 - 3
+        testFullEnclosure[2, 3].player = GameBoard.Player.Player1;
+        testFullEnclosure[3, 2].player = GameBoard.Player.Player1;
+        testFullEnclosure[3, 4].player = GameBoard.Player.Player1;
+        //branches around 5 - 3
+        testFullEnclosure[5, 4].player = GameBoard.Player.Player1;
+        //branches around 7 - 3
+        testFullEnclosure[7, 2].player = GameBoard.Player.Player1;
+        testFullEnclosure[8, 3].player = GameBoard.Player.Player1;
+        //branches around 7 - 5
+        testFullEnclosure[6, 5].player = GameBoard.Player.Player1;
+        testFullEnclosure[8, 5].player = GameBoard.Player.Player1;
+        //branches around 7 - 7
+        testFullEnclosure[7, 8].player = GameBoard.Player.Player1;
+        testFullEnclosure[8, 7].player = GameBoard.Player.Player1;
+        //branches around 5 - 7
+        testFullEnclosure[5, 6].player = GameBoard.Player.Player1;
+        testFullEnclosure[5, 8].player = GameBoard.Player.Player1;
+        //branches around 3 - 7
+        testFullEnclosure[2, 7].player = GameBoard.Player.Player1;
+        testFullEnclosure[3, 8].player = GameBoard.Player.Player1;
+        //branches around 3 - 5
+        testFullEnclosure[3, 4].player = GameBoard.Player.Player1;
+        testFullEnclosure[2, 5].player = GameBoard.Player.Player1;
+        //branches around 5 - 5
+        testFullEnclosure[5, 4].player = GameBoard.Player.Player1;
+        testFullEnclosure[5, 6].player = GameBoard.Player.Player1;
+        testFullEnclosure[6, 5].player = GameBoard.Player.Player1;
+
+        testAI.setCapturedTiles(testAI.AIGameBoard.GameTiles, GameBoard.Player.Player1);
+
+        /*foreach (GameBoard.Tile tile in test1.tileStack)
+        {
+            Debug.Log(tile.coord.x + " - " + tile.coord.y);
+        }
+        Debug.Log(test1.isCaptured);*/
+    }
+}
