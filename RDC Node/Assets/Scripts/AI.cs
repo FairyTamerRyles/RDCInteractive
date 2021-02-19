59f039ffc36756cdfe1f7f6838e019002f31b2d6
FairyTamerRyles <54481765+FairyTamerRyles@users.noreply.github.com>
2021-02-19 12:36:15 -0600

    Revert "Update AI.cs"
    This reverts commit 46fedb0deef7b06a1895ffc9b51f48923809f041, reversing
    changes made to d4a8e9d508bd2af586ad140dc7c815c9c26ffe79.

--- RDC Node/Assets/Scripts/AI.cs
+++ RDC Node/Assets/Scripts/AI.cs
@@ -13,8 +13,6 @@ public class AI
     public GameBoard AIGameBoard;
     
     public struct CapTileChecker
-    //derp
-    private class Vicki
     {
         public List<GameBoard.Tile> tileStack;
         public bool isCaptured;
