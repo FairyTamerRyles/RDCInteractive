ad91103163eb2e22e53501ba6c4c1d4cf4da4ec5
FairyTamerRyles <54481765+FairyTamerRyles@users.noreply.github.com>
2021-04-09 12:51:51 -0500

    File cleanup and action animations

--- RDC Node/Assets/Scripts/Menu/MenuButtonEffectsController.cs
+++ RDC Node/Assets/Scripts/Menu/MenuButtonEffectsController.cs
@@ -14,6 +14,7 @@ public class MenuButtonEffectsController : MonoBehaviour
     {
         GetComponent<RectTransform>().localScale = new Vector3(GetComponent<RectTransform>().localScale.x - .02f, GetComponent<RectTransform>().localScale.x - .02f, 1);   
     }
+
     // Start is called before the first frame update
     void Start()
     {
