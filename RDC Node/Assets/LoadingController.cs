bfb9c334494c91fc38f916dc470bc379cd22c755
FairyTamerRyles <54481765+FairyTamerRyles@users.noreply.github.com>
2021-04-12 15:51:41 -0500

    Orange added to loading screen

--- RDC Node/Assets/LoadingController.cs
+++ RDC Node/Assets/LoadingController.cs
@@ -1,9 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static System.Math;
 
 public class LoadingController : MonoBehaviour
 {
+    public GameObject loader;
+
+    void Start()
+    {
+        float rnd = Random.Range(0.0f, 20.0f);
+        int color = (int)(Floor(rnd));
+        if(color % 2 == 0)
+        {
+            loader.GetComponent<Animator>().SetBool("Orange", true);
+        }
+    }
+
     // Start is called before the first frame update
     public void startGame()
     {
