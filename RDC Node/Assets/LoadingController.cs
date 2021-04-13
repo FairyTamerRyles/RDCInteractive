737bfb9daf561ce7107b76ff025a68b12fb7284a
AdamBJohnson <49881131+AdamBJohnson@users.noreply.github.com>
2021-04-12 23:43:09 -0500

    fixed loading scene

--- RDC Node/Assets/LoadingController.cs
+++ RDC Node/Assets/LoadingController.cs
@@ -5,18 +5,6 @@ using static System.Math;
 
 public class LoadingController : MonoBehaviour
 {
-    public GameObject loader;
-
-    void Start()
-    {
-        float rnd = Random.Range(0.0f, 20.0f);
-        int color = (int)(Floor(rnd));
-        if(color % 2 == 0)
-        {
-            loader.GetComponent<Animator>().SetBool("Orange", true);
-        }
-    }
-
     // Start is called before the first frame update
     public void startGame()
     {
